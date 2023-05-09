6235cb5082611721c4b3e993a680a49f9d49ba93
mohosinmiah <hamza1610330816@gmail.com>
2023-05-10 05:43:44 +0600

    Comment Entity Class Added

---
+++ src/main/java/com/blogapplication/blogapplication/entity/Comment.java
@@ -0,0 +1,37 @@
+package com.blogapplication.blogapplication.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments" )
+public class Comment {
+    
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO
+    )
+    private Long id;
+
+    private String name;
+    private String email;
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false )
+    private Post post;
+}
