0e92f8f3606fecc3545c2c7b15a15c9bcf7adf03
mohosinmiah <hamza1610330816@gmail.com>
2023-05-12 21:56:12 +0600

    Added Validation that we use Entity or DTO

--- src/main/java/com/blogapplication/blogapplication/payload/PostDTO.java
+++ src/main/java/com/blogapplication/blogapplication/payload/PostDTO.java
@@ -1,11 +1,26 @@
 package com.blogapplication.blogapplication.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PostDTO {
-    private Long Id;
+    private long id;
+
+    // title should not be null  or empty
+    // title should have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+    // post description should be not null or empty
+    // post description should have at least 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+
+    // post content should not be null or empty
+    @NotEmpty
     private String content;
 }
