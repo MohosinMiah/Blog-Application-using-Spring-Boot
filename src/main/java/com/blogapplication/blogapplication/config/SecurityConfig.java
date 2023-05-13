854eb7fc2fd17f5f5514845a2f790d2d56596ca3
mohosinmiah <hamza1610330816@gmail.com>
2023-05-13 15:40:35 +0600

    Sample User Create using InMemoryUserDetailsManager

--- src/main/java/com/blogapplication/blogapplication/config/SecurityConfig.java
+++ src/main/java/com/blogapplication/blogapplication/config/SecurityConfig.java
@@ -4,11 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     
+
+    @Bean
+    public static PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
     {
@@ -19,4 +32,24 @@ public class SecurityConfig {
 
         return httpSecurity.build();
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails mohosin = User.builder()
+                                .username("mohosin")
+                                .password(passwordEncoder().encode("mohosin"))
+                                .roles("USER")
+                                .build();
+
+        UserDetails admin = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("admin"))
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(mohosin, admin);
+    }
+
+
 }
