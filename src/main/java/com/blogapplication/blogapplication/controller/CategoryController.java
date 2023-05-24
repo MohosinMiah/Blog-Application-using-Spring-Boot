bbc54a7d2032c0efa53edb04fe75dee66ea04521
mohosinmiah <hamza1610330816@gmail.com>
2023-05-24 21:01:18 +0600

    Category Add Secured By ADMIN ROLE

--- src/main/java/com/blogapplication/blogapplication/controller/CategoryController.java
+++ src/main/java/com/blogapplication/blogapplication/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.blogapplication.blogapplication.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class CategoryController {
     CategoryService categoryService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public CategoryDto addCategory(@RequestBody CategoryDto categoryDto){
         return categoryService.addCategory(categoryDto);
     }
