203bd30b44c17ecd87566264c6763de057b701dd
mohosinmiah <hamza1610330816@gmail.com>
2023-05-24 19:48:19 +0600

    Category Entity and OneToMany and ManyToOne relationship

---
+++ src/main/java/com/blogapplication/blogapplication/entity/Category.java
@@ -0,0 +1,36 @@
+package com.blogapplication.blogapplication.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.AUTO
+    )
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true )
+    private List<Post> posts;
+
+}
--- src/main/java/com/blogapplication/blogapplication/entity/Post.java
+++ src/main/java/com/blogapplication/blogapplication/entity/Post.java
@@ -3,13 +3,15 @@ package com.blogapplication.blogapplication.entity;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -56,5 +58,9 @@ public class Post {
 
     @OneToMany(mappedBy="post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Comment> comments = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 
 }
