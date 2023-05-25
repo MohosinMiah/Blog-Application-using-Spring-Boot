a207296101b53084b753cb9bb62fbf0ad5f55f09
mohosinmiah <hamza1610330816@gmail.com>
2023-05-26 05:53:42 +0600

    Spring Boot OpenAI API Documentation Dependency and Configuration Added

--- src/main/java/com/blogapplication/blogapplication/config/SecurityConfig.java
+++ src/main/java/com/blogapplication/blogapplication/config/SecurityConfig.java
@@ -79,6 +79,8 @@ public class SecurityConfig {
         .authorizeHttpRequests( requests -> requests
             .requestMatchers(HttpMethod.GET,  "/api/**").authenticated()
             .requestMatchers("/api/auth/**").permitAll()
+            .requestMatchers("/swagger-ui/**").permitAll()
+            .requestMatchers("/v3/api-docs/**").permitAll()
             .anyRequest().authenticated()
         ).exceptionHandling( exception -> exception
             .authenticationEntryPoint(jwtAuthenticationEntryPoint)
