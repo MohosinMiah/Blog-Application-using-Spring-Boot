0fa0bf6a9b82298f4c5b2ebea7dbcf973b7328b9
mohosinmiah <hamza1610330816@gmail.com>
2023-05-26 21:39:52 +0600

    Swagger model documentation

--- src/main/java/com/blogapplication/blogapplication/payload/PostDTO.java
+++ src/main/java/com/blogapplication/blogapplication/payload/PostDTO.java
@@ -1,11 +1,15 @@
 package com.blogapplication.blogapplication.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(
+    description = "Post DTO Model Class"
+)
 public class PostDTO {
     private long id;
 
@@ -13,18 +17,31 @@ public class PostDTO {
     // title should have at least 2 characters
     @NotEmpty
     @Size(min = 2, message = "Post title should have at least 2 characters")
+    @Schema(
+        description = "Blog Post Title"
+    )
     private String title;
 
     // post description should be not null or empty
     // post description should have at least 10 characters
+
     @NotEmpty
     @Size(min = 10, message = "Post description should have at least 10 characters")
+    @Schema(
+        description = "Blog Post Description"
+    )
     private String description;
 
     // post content should not be null or empty
+    @Schema(
+        description = "Blog Post Content"
+    )
     @NotEmpty
     private String content;
 
     @NotNull
+    @Schema(
+        description = "Blog Post Category ID"
+    )
     private Long categoryId;
 }
