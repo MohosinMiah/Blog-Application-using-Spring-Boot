fedf1b91ea3fd9b425419b8dc00704a314421b3d
mohosinmiah <hamza1610330816@gmail.com>
2023-05-26 06:18:47 +0600

    API Document Information Using Swagger Annotation

--- src/main/java/com/blogapplication/blogapplication/BlogapplicationApplication.java
+++ src/main/java/com/blogapplication/blogapplication/BlogapplicationApplication.java
@@ -3,7 +3,31 @@ package com.blogapplication.blogapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Spring Boot Blog App Rest API",
+		description = "Spring Boot Blog APIS Documentations",
+		version = "v1.0",
+		contact = @Contact(
+			name = "MD MOHOSIN MIAH",
+			email = "mohosin.miah@bd.adventurekk.com"
+		),
+		license = @License(
+			name = "ApacheFree"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		description = "This is Blog App developed by Fun Guy",
+		url = "https://github.com/MohosinMiah"
+	)
+)
 public class BlogapplicationApplication {
 
 	public static void main(String[] args) {
